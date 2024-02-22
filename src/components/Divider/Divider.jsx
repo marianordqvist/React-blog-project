e5de4d695b7e365aa163c92ab1e35f43dd12ea92
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-02-22 10:41:35 +0100

    Divider, featuredblogpost hero

---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,16 @@
+import "./Divider.css";
+function Divider({ text }) {
+  return (
+    <div className="divider" role="page divider">
+      <div className="line"></div>
+      <h2 className="divider--titel">{text}</h2>
+      <div className="line"></div>
+    </div>
+  );
+}
+
+export default Divider;
+
+// in app.jsx
+// <Divider text={"Featured Blog Post"} />
+// <Divider text={"Blog Post"} />
