d6d84a92773e7edd3a75311ddab096b00e880808
setayeshnri <setayeshn1382@gmail.com>
2024-02-22 16:40:09 +0100

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
   return <>
   <Banner />
+  <Footer />
   </>;
 }
 
