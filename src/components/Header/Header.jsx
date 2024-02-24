e5f4f512f7233287ebaf5814c6a4a5a98731263f
Hifza Batool <107994867+HifzaBatool@users.noreply.github.com>
2024-02-24 10:42:47 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 // components/Header.jsx
 import React,{ useState} from 'react';
 import  './Header.css';
+import ReactJS from './ReactJS.gif';
 
 const Header = () => {
   const [isNavActive, setNavActive] = useState(false);
@@ -10,21 +11,25 @@ const Header = () => {
   };
 
   return (
-    <header className='globel-header'>
+    <header className='global-header'>
       <nav className='nav-bar'>
-      <input type="checkbox" id="check" className="hamburger-checkbox" onChange={toggleNav} />
-      <label htmlFor="check" className={`checkbtn ${isNavActive ? 'active' : ''}`}>
+      <input type="checkbox" 
+      id="check" className="hamburger-checkbox"
+       onChange={toggleNav} 
+        aria-hidden="true" />
+      <label htmlFor="check" className={`checkbtn ${isNavActive ? 'active' : ''}`} aria-label="Toggle Navigation">
         <i className='fas fa-bars'></i>
       </label>
-      <label className="logo">The Reactor</label>
-     
 
+      <label className="logo">
+        <img src={ReactJS} alt="React Logo" className='react-logo'/>
+        The Reactor</label>
       
         <ul >
           <li ><a  className='active' href="#home">Home</a></li>
           <li ><a  className='active' href="#about">About</a></li>
           <li ><a  className='active' href="#blog">Blog</a></li>
-          <li ><a   className='active' href="#tutorials">Sign Up</a></li>
+          <li ><a   className='active' href="#signsup">Sign Up</a></li>
           <li ><a className='active' href="#resources">Resources</a></li>
           <li ><a className='active' href="#contact">Contact</a></li>
         </ul>
